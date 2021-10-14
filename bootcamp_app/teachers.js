860e67eef27e8ceac59fdd81b38f17647436c772
habibcodes <habib.codes@gmail.com>
2021-10-13 22:56:34 -0400

    create queries using Postgres package to create connection via JS to the DB

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,40 @@
+/* eslint-disable indent */
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
+  host: 'localhost',
+  database: 'bootcampx',
+  port: 5432,
+});
+
+
+const sqlQuery = `
+  SELECT DISTINCT 
+    teachers.name as teacher, 
+    cohorts.name as cohort
+  FROM 
+    teachers
+  JOIN 
+    assistance_requests 
+      ON teacher_id = teachers.id 
+  JOIN 
+    students 
+      ON student_id = students.id 
+  JOIN 
+    cohorts 
+      ON cohort_id = cohorts.id 
+  WHERE 
+    cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+  ORDER BY 
+    teacher;
+`;
+
+pool
+  .query(sqlQuery)
+  .then(res => {
+    res.rows.forEach(row => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  }).catch(err => console.error('query error', err.stack));
